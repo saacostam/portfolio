37b107ead90df975d99e93b0e0c0bc01788f3d76
Santiago Acosta <122395802+saacostam-zaga@users.noreply.github.com>
2024-11-02 21:29:25 -0500

    feat: add link to new porfolio

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,21 @@
 <template>
-  <NavbarVue id="navbar"/>
+  <!-- <NavbarVue id="navbar"/>
   <div id="non-navbar">
     <BrandVue id="brand"/>
     <router-view/>
-  </div>
+  </div> -->
+  <main></main>
 </template>
 
 <script>
-import NavbarVue from './components/Navbar.vue'
-import BrandVue from './components/Brand.vue'
+// import NavbarVue from './components/Navbar.vue'
+// import BrandVue from './components/Brand.vue'
 export default{
   components:{
-    NavbarVue,
-    BrandVue
+    // NavbarVue,
+    // BrandVue
   }
 }
 </script>
 
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
-:root {
-  font-size: 16px;
-  font-family: 'Open Sans', sans-serif;
-  --white: white;
-  --blue: #232323;
-  --orange: #232323;
-  --transition-speed: 500ms;
-}
-body{
-  background-color: #2f2f2f;
-}
-#navbar{
-  width: 4rem;
-  height: 100vh;
-  z-index: 1000;
-}
-#navbar:hover{
-  width: 12rem;
-}
-#non-navbar{
-  margin-left: 4rem;
-}
-#brand{
-  height: 4rem;
-}
-*{
-  font-weight: 300;
-  color: white;
-}
-</style>
+<style></style>
